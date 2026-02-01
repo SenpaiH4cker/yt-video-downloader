b92d46cfb55b41d5c8c30de56f17c5bc232f232d
SenpaiH4cker <developerhollow@gmail.com>
2026-02-01 18:15:13 +0100

    yt video downloader

--- yt.py
+++ yt.py
@@ -3,7 +3,8 @@
 import yt_dlp
 url = input("ENTER URL :")
 ydl_opts = {
-    'outtmpl': r'C:\\Users\\khoul\\Downloads\\hazymoon.%(ext)s',
+    'outtmpl': r'C:\\Users\\khoul\\Downloads\\example.%(ext)s',
 } #to save the video
 with yt_dlp.YoutubeDL(ydl_opts) as yt:
+
     yt.download([url])
